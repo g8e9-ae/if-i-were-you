39212fe95c3f3b3585bd5fd978bd2403a2f1b961
Akira Endo <a_endo@ga-tech.co.jp>
2019-06-19 10:20:42 +0900

    New generate-dummy-file.sh

---
+++ shell-script/generate-dummy-file.sh
@@ -0,0 +1,21 @@
+
+
+
+app_dir=$( cd $( dirname $BASH_SOURCE ) && pwd )
+
+if [[ $# < 1 ]]; then
+  echo -e "\n\tusage: $0 <number-of-files>"
+  exit 0
+fi
+
+output_dir=$app_dir/out
+number_of_days=$1
+
+if [[ ! -e $output_dir ]]; then
+  mkdir  $output_dir || exit 1
+fi
+
+for i in `seq ${number_of_days}`; do
+  date_string=`date -v+${i}d +'%Y%m%d' `
+  touch $output_dir/$date_string.txt
+done
